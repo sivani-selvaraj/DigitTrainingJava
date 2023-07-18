202627c04e528de17536807aaa100f3e6366dfdc
Sivani.S <Sivani.S@DIGTBGL-L6981.godigit.corp>
2023-07-18 12:30:21 +0530

    THis is my second commit

--- src/main/java/com/digit/testProj/p1/TestingCode.java
+++ src/main/java/com/digit/testProj/p1/TestingCode.java
@@ -2,7 +2,7 @@ package com.digit.testProj.p1;
 
 public class TestingCode {
 	public static void main(String[] args) {
-		System.out.println("Im SIvani");
+		System.out.println("I'm Sivani");
 	}
 
 }
